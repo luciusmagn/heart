46ba54a8bebb8dd94dec7261283421a1f48eb7ea
Lukáš Hozda <luk.hozda@gmail.com>
2024-11-18 15:26:21 +0100

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use ergokv::Store;
+pub use maud::{html, Markup};
+pub use warp::{Filter, Rejection, Reply};
